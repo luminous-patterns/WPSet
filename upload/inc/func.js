5fb1085678e2186a367d3774afed2d85926a48e7
Callan Milne <callan.milne@integratedweb.com.au>
2011-10-31 19:58:34 +1100

    Initial commit of beta 1.0.0

---
+++ upload/inc/func.js
@@ -0,0 +1,20 @@
+
+jQuery( document ).ready( function() {
+	if ( jQuery( 'div.wpset-tabbed-settings' ).length > 0 ) {
+		jQuery( 'div.wpset-tabbed-settings' ).each( function() {
+			var these_settings = jQuery( this );
+			var settings_tabs = these_settings.find( 'ul.settings-tabs li.tab');
+			var settings_pages = these_settings.find( 'div.settings-sections div.group' );
+			if ( settings_tabs.length > 0 ) {
+				settings_tabs.click( function() {
+					var match = /group\-([a-z-]+)/.exec( jQuery( this ).attr( 'class' ) );
+					var page_name = match[1];
+					settings_pages.addClass( 'hidden' );
+					these_settings.find( 'div.settings-sections div.group-' + page_name ).removeClass( 'hidden' );
+					settings_tabs.removeClass( 'selected' );
+					jQuery( this ).addClass( 'selected' );
+				} );
+			}
+		} );
+	}
+} );
